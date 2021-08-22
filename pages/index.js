727e3da2fc04079418a84eb4b0e032cdded86aae
Patrick <patrick.suehrer@thedac.info>
2021-08-22 19:17:36 +0200

    project init

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Facebook 2.0</title>
+      </Head>
+
+      <Header/>
+
+      <main>
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </main>
+    </div>
+  )
+}
